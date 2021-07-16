0b7a732d9c2570474d7322331845212462eee2f3
aaglazkov <aaglazkov@luxoft.com>
2021-07-16 12:11:15 +0600

    rename url rest

--- src/main/java/com/factory/deepforestrunner/rest/DeepController.java
+++ src/main/java/com/factory/deepforestrunner/rest/DeepController.java
@@ -22,9 +22,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class DeepController {
 
-    @GetMapping("/hello")
+    @GetMapping("/runner")
     public String hello(
-        @RequestParam(value = "name", defaultValue = "World") String name
+        @RequestParam(value = "name") String name
     ) {
         return String.format("Hello %s!", name);
     }
