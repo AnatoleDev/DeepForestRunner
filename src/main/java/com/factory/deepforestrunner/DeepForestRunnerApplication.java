9f829bcc32c247c1166bd3c4d9d9f21aed9b8cf3
aaglazkov <aaglazkov@luxoft.com>
2021-07-17 23:24:11 +0600

    add start schema

--- src/main/java/com/factory/deepforestrunner/DeepForestRunnerApplication.java
+++ src/main/java/com/factory/deepforestrunner/DeepForestRunnerApplication.java
@@ -1,28 +1,12 @@
 package com.factory.deepforestrunner;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.jdbc.core.JdbcTemplate;
 
 @SpringBootApplication
-public class DeepForestRunnerApplication implements CommandLineRunner {
-
-    @Autowired
-    private JdbcTemplate jdbcTemplate;
+public class DeepForestRunnerApplication {
 
     public static void main(String... args) {
         SpringApplication.run(DeepForestRunnerApplication.class, args);
     }
-
-    @Override
-    public void run(String... args) {
-        //Create the database table:
-        jdbcTemplate.execute("CREATE TABLE IF NOT EXISTS runners(" +
-            "id INTEGER PRIMARY KEY," +
-            " name VARCHAR(100) NOT NULL" +
-            ")");
-    }
-
 }
